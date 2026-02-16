0d47bc918b53f602b715af921c6b57a635b395f4
mathieu <mathieu.beriac@ynov.com>
2026-02-16 09:15:10 +0100

    feat: Push branch users and information for users

---
+++ src/main/java/fr/ynov/kanoe/model/Utilisateur.java
@@ -0,0 +1,52 @@
+public class Utilisateur {
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private String motDePasse;
+    private int id;
+    
+    public Utilisateur(String nom, String prenom, String email, String telephone, String motDePasse, int id) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.telephone = telephone;
+        this.motDePasse = motDePasse;
+        this.id = id;
+    }
+
+    
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    public boolean verifierMotDePasse(String motDePasse) {
+        return this.motDePasse.equals(motDePasse);
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "Utilisateur{" +
+                "nom='" + nom + '\'' +
+                ", email='" + email + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
