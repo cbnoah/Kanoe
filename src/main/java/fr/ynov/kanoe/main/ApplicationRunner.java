d4f26b5bb0e4d9b67989d06e21e5a137c040dc9c
cnoah <noah.charrinbourrat@ynov.com>
2026-02-18 00:04:43 +0100

    feat: initialize transport system with dummy data in ApplicationRunner

--- src/main/java/fr/ynov/kanoe/main/ApplicationRunner.java
+++ src/main/java/fr/ynov/kanoe/main/ApplicationRunner.java
@@ -1,17 +1,59 @@
 package main.java.fr.ynov.kanoe.main;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import main.java.fr.ynov.kanoe.model.Avion;
+import main.java.fr.ynov.kanoe.model.Bus;
+import main.java.fr.ynov.kanoe.model.Train;
+import main.java.fr.ynov.kanoe.service.SystemeReservation;
+
+import java.time.LocalDateTime;
+
+import static main.java.fr.ynov.kanoe.cli.Menu.displayMainMenu;
+
 public class ApplicationRunner {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        SystemeReservation system = new SystemeReservation();
+        // Creating some dummy data for testing
+        system.ajouterTransport(new Avion.Builder()
+                .setId("AV001")
+                .setOperator("Air France")
+                .setStatingPoint("Paris")
+                .setEndPoint("New York")
+                .setTimeDepart(LocalDateTime.parse("2024-07-01T10:00:00"))
+                .setTimeArriving(LocalDateTime.parse("2024-07-01T14:00:00"))
+                .setCapacity(400)
+                .setBasePrice(500.0)
+                .setPlaneNumber("AF001")
+                .setInFlightEntertainment(true)
+                .build());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        system.ajouterTransport(new Train.Builder()
+                .setId("TR001")
+                .setOperator("SNCF")
+                .setStatingPoint("Paris")
+                .setEndPoint("Lyon")
+                .setTimeDepart(LocalDateTime.parse("2024-07-01T08:00:00"))
+                .setTimeArriving(LocalDateTime.parse("2024-07-01T10:00:00"))
+                .setCapacity(300)
+                .setBasePrice(100.0)
+                .setTrainNumber("SNCF001")
+                .setHasWifi(true)
+                .setHasDiningCar(true)
+                .build());
+
+        system.ajouterTransport(new Bus.Builder()
+                .setId("BS001")
+                .setOperator("FlixBus")
+                .setStatingPoint("Paris")
+                .setEndPoint("Brussels")
+                .setTimeDepart(LocalDateTime.parse("2024-07-01T09:00:00"))
+                .setTimeArriving(LocalDateTime.parse("2024-07-01T12:00:00"))
+                .setCapacity(50)
+                .setBasePrice(30.0)
+                .setBusNumber("FLX001")
+                .setAirConditioning(true)
+                .build());
+
+        // Display the main menu
+        displayMainMenu(system);
     }
 }
