4921c02be60503d0eba38a330dc5bd694d3b5146
cnoah <noah.charrinbourrat@ynov.com>
2026-02-18 23:41:20 +0100

    refactor: rename Users and SystemeReservation to User and BookingSystem for consistency

--- src/main/java/fr/ynov/kanoe/utils/NotificationCreator.java
+++ src/main/java/fr/ynov/kanoe/utils/NotificationCreator.java
@@ -1,36 +1,36 @@
 package main.java.fr.ynov.kanoe.utils;
 
-import main.java.fr.ynov.kanoe.model.Reservation;
-import main.java.fr.ynov.kanoe.model.Users;
+import main.java.fr.ynov.kanoe.model.Booking;
+import main.java.fr.ynov.kanoe.model.User;
 import main.java.fr.ynov.kanoe.service.Notification;
-import main.java.fr.ynov.kanoe.service.SystemeReservation;
+import main.java.fr.ynov.kanoe.service.BookingSystem;
 
 import java.util.List;
 
 public class NotificationCreator {
-    private final SystemeReservation system;
+    private final BookingSystem system;
 
-    public NotificationCreator(SystemeReservation system) {
+    public NotificationCreator(BookingSystem system) {
         this.system = system;
     }
 
     public void createNotification(String title, String message, String scope) {
-        for (Users user: getUserById(getUserIdForScopedUsers(scope)))
+        for (User user: getUserById(getUserIdForScopedUsers(scope)))
         {
             system.getNotificationManager().notifyObserver(user, new Notification(title, message, scope));
         }
     }
 
     public int getUserIdForScopedUsers(String scope) {
-        for (Reservation reservation: system.getReservations()) {
-            if (reservation.getTransport().getId().equals(scope)) {
-                return reservation.getUserId();
+        for (Booking booking : system.getReservations()) {
+            if (booking.getTransport().getId().equals(scope)) {
+                return booking.getUserId();
             }
         }
         return 0;
     }
 
-    public List<Users> getUserById(int userId) {
+    public List<User> getUserById(int userId) {
         return system.getUtilisateurs().stream()
                 .filter(u -> u.getId() == userId)
                 .toList();
