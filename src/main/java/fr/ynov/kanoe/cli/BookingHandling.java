c4e27f8df4761bd0a3c52d4c3a0df7cdc2fac894
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-02-18 19:15:55 +0100

    feat: add BookingHandling class for managing reservations

---
+++ src/main/java/fr/ynov/kanoe/cli/BookingHandling.java
@@ -0,0 +1,44 @@
+package main.java.fr.ynov.kanoe.cli;
+
+import main.java.fr.ynov.kanoe.enums.StatusReservation;
+import main.java.fr.ynov.kanoe.model.Reservation;
+
+import java.util.Scanner;
+
+public class BookingHandling {
+    Reservation reservation;
+
+    public BookingHandling(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+    public void ManageOneBooking() {
+        Scanner scanner = new Scanner(System.in);
+        if (reservation.getStatut() == StatusReservation.CANCELED) {
+            System.out.println("This reservation is already canceled.");
+            return;
+        }
+        if (this.reservation.getStatut() == StatusReservation.SUCCESS) {
+            System.out.println("This reservation is already confirmed.");
+            return;
+        }
+        System.out.println("=== Booking management ===");
+        System.out.println("1. Confirm reservation");
+        System.out.println("2. Cancel reservation");
+        System.out.println("3. Back to user menu");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1 -> {
+                if (reservation.processPayment()) {
+                    System.out.println("Reservation confirmed!");
+                } else {
+                    System.out.println("Payment failed, reservation not confirmed.");
+                }
+            }
+            case 2 -> {
+                reservation.annuler();
+                System.out.println("Reservation canceled.");
+            }
+        }
+    }
+}
